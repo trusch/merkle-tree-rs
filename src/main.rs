47e78792a64a08f1cdc3f98deabc0ed2ea239ed5
Tino Rusch <tino@kilt.io>
2024-02-21 12:27:17 +0100

    add a minimal main, because why not

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
 mod merkle_tree;
 
+use merkle_tree::MerkleTree;
+
 fn main() {
-    println!("Hello, world!");
+
+     let initial_value = [0x00; 32];
+    let mut tree = MerkleTree::new(5, &initial_value);
+    for i in 0..tree.num_leaves() {
+        let updated_value = [(i * 0x11) as u8; 32];
+        tree.set(i, &updated_value);
+    }
+    let leaf_5 = [5 * 0x11 as u8; 32];
+    let root = tree.root_hash();
+    let proof = tree.create_proof(5);
+    assert_eq!(&tree.verify_proof(&leaf_5, &proof), root);
 }
